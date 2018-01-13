c49a124601ce396d6f9cbf775cbea04e36544999
Jailani Abdul Rahman <jailani.har@googlemail.com>
2018-01-13 22:13:52 +0800

    Week 1 Overview of Java (Slot 2)

--- week1/carapplication/RunProgram.java
+++ week1/carapplication/RunProgram.java
@@ -3,18 +3,25 @@ package week1.carapplication;
 public class RunProgram {
 
 	public static void main(String[] args) {
+		Wheels wheels1 = new Wheels();
+		wheels1.brand = "Continental";
+		wheels1.size = 18;
+		wheels1.brakeSpeed = 4;
 		Car car1 = new Car();
 		car1.brand = "Ford";
 		car1.model = "Focus ST";
 		car1.colour = "Blue";
+		car1.wheels = wheels1;
 		Car car2 = new Car();
 		car2.brand = "Kia";
 		car2.model = "Stinger";
 		car2.colour = "Black";
+		car2.wheels = wheels1;
 		
 		printCarDetails(car1);
 		printCarDetails(car2);
 		car1.colour = "Black";
+		car1.wheels.brand = "Pirelli";
 		printCarDetails(car1);
 		printCarDetails(car2);
 		car1.pressAccelerate();
@@ -28,6 +35,9 @@ public class RunProgram {
 				+ car.model + " "
 				+ car.colour + " (Current Speed: "
 				+ car.currentSpeed + ")");
+		System.out.println(car.wheels.brand + " size "
+				+ car.wheels.size + " brake speed "
+				+ car.wheels.brakeSpeed);
 	}
 	
 }
