5ce5064f5a55f2135ae7af1ebc23f50fc4dd4399
Jailani Abdul Rahman <jailani.har@googlemail.com>
2018-03-14 16:38:10 +0800

    Week 10 Java Socket (Slot 1)

---
+++ week10/learningserverclientapp/Client.java
@@ -0,0 +1,30 @@
+package week10.learningserverclientapp;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+	public static void main(String[] args) {
+		try {
+			Socket socket = new Socket("localhost", 9101);
+			System.out.println("Connected to the server.");
+			
+			DataOutputStream toServer =
+					new DataOutputStream(socket.getOutputStream());
+			
+			Scanner scanner = new Scanner(System.in);
+			
+			while(true) {
+				String message = scanner.nextLine();
+				toServer.writeUTF(message);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
